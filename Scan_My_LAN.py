6d76a58e6668507d2e18f8635d2263a7089ae239
DamienDaco <dacodamien@gmail.com>
2018-05-27 13:56:08 +0200

    Changing the app structure to MVC structure. First step of many.

--- Scan_My_LAN.py
+++ Scan_My_LAN.py
@@ -1,10 +1,18 @@
 import sys
 from app.model import *
+from ui.Scan_My_LAN import *
+from app.view import *
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
 
 
 if __name__ == "__main__":
 
     app = QApplication(sys.argv)
+
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     model = Model()
     sys.exit(app.exec_())
--- app/model.py
+++ app/model.py
@@ -1,5 +1,5 @@
 from app.network_functions import *
-from app.view import MainWindow
+# from app.view import MainWindow
 
 
 class Model:
@@ -14,16 +14,16 @@ class Model:
 
         self.interface_list = get_interfaces()
 
-        self.main_window = MainWindow()
-        self.main_window.show()
+        # self.main_window = MainWindow()
+        # self.main_window.show()
 
-        self.main_window.interface_box.addItems(self.interface_list)
-        self.main_window.interface_box.setCurrentText(self.default_interface)
+        # self.main_window.interface_box.addItems(self.interface_list)
+        # self.main_window.interface_box.setCurrentText(self.default_interface)
 
 
 
         print("Your default interface is {}, your IP is {}, your mask is {} and your MAC is {}".format(
               self.default_interface, self.my_ip, self.my_mask, self.my_mac))
 
-        # calc_range(self.my_ip, self.my_mask)
-        self.main_window.start_button.clicked.connect(lambda: calc_range(self.my_ip, self.my_mask))
+        # # calc_range(self.my_ip, self.my_mask)
+        # self.main_window.start_button.clicked.connect(lambda: calc_range(self.my_ip, self.my_mask))
